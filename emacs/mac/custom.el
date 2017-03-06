aa7b0f186c25da3476523f77c8e2001fe5fca485
Ye Binbin <phantom2501@gmail.com>
2017-03-06 23:24:36 +0900

    Update config

--- emacs/mac/custom.el
+++ emacs/mac/custom.el
@@ -27,21 +27,23 @@
 ;; (define-key helm-map (kbd "RET") 'helm-execute-persistent-action) ; rebind return to do persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-(setq mac-command-modifier 'super)
-(setq mac-option-modifier 'meta)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
 
-(global-set-key (kbd "<C-f2>") 'bm-toggle)
+(global-set-key (kbd "C-;") 'bm-toggle)
 (global-set-key (kbd "<f2>") 'bm-next)
-(global-set-key (kbd "<S-f2>") 'bm-previous)
+(global-set-key (kbd "s-<f2>") 'bm-previous)
 
+(global-set-key (kbd "C-.") 'helm-projectile-ag)
+(global-set-key (kbd "C-,") 'helm-projectile-find-file)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;; Package settings ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Use X window clipboard
 ;; (xclip-mode t)
 
 ;; (global-highlight-parentheses-mode t)
 ;; Disable smartparens, it will freeze emacs
-;; (add-hook 'prelude-prog-mode-hook (lambda () (smartparens-mode -1)) t)
-;; (show-smartparens-global-mode -1)
+(add-hook 'prelude-prog-mode-hook (lambda () (smartparens-mode -1)) t)
+(show-smartparens-global-mode -1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;; Auto Generated ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
@@ -51,7 +53,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (bm zop-to-char zenburn-theme yaml-mode which-key volatile-highlights vkill undo-tree smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters qml-mode ov operate-on-number move-text markdown-mode magit json-mode js2-mode imenu-anywhere ido-ubiquitous helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region exec-path-from-shell elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux company-anaconda browse-kill-ring beacon anzu ace-window))))
+    (markdown-mode editorconfig zop-to-char zenburn-theme which-key volatile-highlights vkill undo-tree smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters qml-mode ov operate-on-number move-text magit json-mode js2-mode imenu-anywhere ido-ubiquitous helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region exec-path-from-shell elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux company-anaconda browse-kill-ring bm beacon anzu ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
