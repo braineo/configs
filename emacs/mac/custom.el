c51312d1d7e89ecc544686dd577f65441e3b3e93
Ye Binbin <phantom2501@gmail.com>
2017-05-24 20:53:49 +0900

    Disable elpy due to performance issue

--- emacs/mac/custom.el
+++ emacs/mac/custom.el
@@ -4,7 +4,7 @@
 
 ;;; Code:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Packages ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(prelude-require-packages '(qml-mode bm nlinum highlight-parentheses elpy))
+(prelude-require-packages '(qml-mode bm nlinum highlight-parentheses highlight-indentation))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Prelude ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Disable whitespace mode
@@ -33,9 +33,9 @@
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 
-(global-set-key (kbd "C-;") 'bm-toggle)
-(global-set-key (kbd "M-;") 'bm-next)
-(global-set-key (kbd "s-;") 'bm-previous)
+;; (global-set-key (kbd "C-;") 'bm-toggle)
+;; (global-set-key (kbd "M-;") 'bm-next)
+;; (global-set-key (kbd "s-;") 'bm-previous)
 
 (global-set-key (kbd "C-.") 'helm-projectile-ag)
 (global-set-key (kbd "C-,") 'helm-projectile-find-file)
@@ -52,14 +52,18 @@
 (show-smartparens-global-mode -1)
 
 
-(elpy-enable)
-(elpy-use-ipython)
+;; (elpy-enable)
+;; (elpy-use-ipython)
+;; (setq elpy-rpc-backend "jedi")
+(add-hook 'python-mode-hook 'highlight-indentation-mode)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;; Auto Generated ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elpy-eldoc-show-current-function t)
+ '(elpy-rpc-backend "jedi")
  '(package-selected-packages
    (quote
     (zop-to-char zenburn-theme yaml-mode which-key volatile-highlights vkill undo-tree swift-mode smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters qml-mode ov operate-on-number nlinum move-text markdown-mode magit json-mode js2-mode imenu-anywhere ido-ubiquitous helm-projectile helm-descbinds helm-ag guru-mode grizzl gotest god-mode go-projectile gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region exec-path-from-shell elpy elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux company-go company-anaconda browse-kill-ring bm beacon anzu ace-window))))
