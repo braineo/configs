2fbd92f5573cc672855188e1941ddf7888630b90
Ye Binbin <phantom2501@gmail.com>
2017-06-04 15:46:40 +0900

    Update autocomplete for c/c++

--- emacs/personal/setup-c-mode.el
+++ emacs/personal/setup-c-mode.el
@@ -1,11 +1,37 @@
-(prelude-require-packages '(company-c-headers helm-gtags semantic ede ggtags))
+(prelude-require-packages '(company-c-headers helm-gtags function-args cc-mode irony))
 
-(add-to-list 'company-backends 'company-c-headers)
+(require 'company)
+(add-to-list 'company-backends '(company-c-headers company-irony))
+(setq company-backends (delete 'company-semantic company-backends))
 
-;;; Enable helm-gtags-mode
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-(add-hook 'c++-mode-hook 'helm-gtags-mode)
-(add-hook 'asm-mode-hook 'helm-gtags-mode)
+;; cmake command to build irony
+;; cmake -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm@3.9/bin/clang++ -DCMAKE_PREFIX_PATH=/usr/local/opt/llvm@3.9 -DCMAKE_INSTALL_RPATH=/usr/local/opt/llvm@3.9 -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE -DCMAKE_INSTALL_PREFIX\=/Users/braineo/.emacs.d/irony/ /Users/braineo/.emacs.d/elpa/irony-20170523.618/server && cmake --build . --use-stderr --config Release --target install
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'swift-mode 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; Function args mode setup
+(fa-config-default)
+
+;; setup GDB
+
+(setq
+ ;; use gdb-many-windows by default
+ gdb-many-windows t
+
+ ;; Non-nil means display source file containing the main routine at startup
+ gdb-show-main t)
+
+;; ========= helm-gtags ==========
+(require 'helm-gtags)
+(setq helm-gtags-ignore-case t
+      helm-gtags-auto-update t
+      helm-gtags-use-input-at-cursor t
+      helm-gtags-pulse-at-cursor t
+      helm-gtags-prefix-key "\C-cg"
+      helm-gtags-suggested-key-mapping t)
 
 ;; customize
 (custom-set-variables
@@ -13,62 +39,28 @@
  '(helm-gtags-ignore-case t)
  '(helm-gtags-auto-update t))
 
-;; Enable EDE only in C/C++
-(global-ede-mode)
+;; Enable helm-gtags-mode in Dired so you can jump to any tag
+;; when navigate project tree with Dired
+(add-hook 'dired-mode-hook 'helm-gtags-mode)
 
-;; setup GDB
-(setq
- ;; use gdb-many-windows by default
- gdb-many-windows t
+;; Enable helm-gtags-mode in Eshell for the same reason as above
+(add-hook 'eshell-mode-hook 'helm-gtags-mode)
 
- ;; Non-nil means display source file containing the main routine at startup
- gdb-show-main t
- )
+;; Enable helm-gtags-mode in languages that GNU Global supports
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'java-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
 
- (ggtags-mode 1)
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
-              (ggtags-mode 1))))
+;; Helm--gtags shortcuts
+(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
 
-(dolist (map (list ggtags-mode-map dired-mode-map))
-  (define-key map (kbd "C-c g s") 'ggtags-find-other-symbol)
-  (define-key map (kbd "C-c g h") 'ggtags-view-tag-history)
-  (define-key map (kbd "C-c g r") 'ggtags-find-reference)
-  (define-key map (kbd "C-c g f") 'ggtags-find-file)
-  (define-key map (kbd "C-c g c") 'ggtags-create-tags)
-  (define-key map (kbd "C-c g u") 'ggtags-update-tags)
-  (define-key map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-  (define-key map (kbd "M-.") 'ggtags-find-tag-dwim)
-  (define-key map (kbd "M-,") 'pop-tag-mark)
-  (define-key map (kbd "C-c <") 'ggtags-prev-mark)
-  (define-key map (kbd "C-c >") 'ggtags-next-mark))
-
-
-(setq helm-gtags-ignore-case t
-          helm-gtags-auto-update t
-          helm-gtags-use-input-at-cursor t
-          helm-gtags-pulse-at-cursor t
-          helm-gtags-prefix-key "\C-cg"
-          helm-gtags-suggested-key-mapping t)
-
-    ;; Enable helm-gtags-mode in Dired so you can jump to any tag
-    ;; when navigate project tree with Dired
-    (add-hook 'dired-mode-hook 'helm-gtags-mode)
-
-    ;; Enable helm-gtags-mode in Eshell for the same reason as above
-    (add-hook 'eshell-mode-hook 'helm-gtags-mode)
-
-    ;; Enable helm-gtags-mode in languages that GNU Global supports
-    (add-hook 'c-mode-hook 'helm-gtags-mode)
-    (add-hook 'c++-mode-hook 'helm-gtags-mode)
-    (add-hook 'java-mode-hook 'helm-gtags-mode)
-    (add-hook 'asm-mode-hook 'helm-gtags-mode)
-
-(with-eval-after-load 'helm-gtags
-      (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-      (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
-      (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-      (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-      (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-      (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history))
+(global-set-key (kbd "<f5>") (lambda ()
+                               (interactive)
+                               (setq-local compilation-read-command nil)
+                               (call-interactively 'compile)))
