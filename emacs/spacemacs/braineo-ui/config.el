ba4fe074e70eb64fbc1c3bdf71c4dd9907db0de9
Binbin Ye <binbin.ye@git.mujin.co.jp>
2017-11-13 14:30:15 +0900

    Do not load background when emacs starts in terminal

--- emacs/spacemacs/braineo-ui/config.el
+++ emacs/spacemacs/braineo-ui/config.el
@@ -2,3 +2,11 @@
 ;; Set editor transparency
 (set-frame-parameter (selected-frame) 'alpha '(92 92))
 (add-to-list 'default-frame-alist '(alpha 92 92))
+
+
+(defun on-frame-open (&optional frame)
+  ;; "If the FRAME created in terminal don't load background color."
+  (unless (display-graphic-p frame)
+    (set-face-background 'default "unspecified-bg" frame)))
+
+(add-hook 'after-make-frame-functions 'on-frame-open)
