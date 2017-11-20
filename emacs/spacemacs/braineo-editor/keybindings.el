d359e08d94d73117c8b0fde957cbaaf304a98fd0
Binbin Ye <binbin.ye@mujin.co.jp>
2017-11-20 15:34:02 +0900

    Add key binding for folding and use anzu mode for replace

--- emacs/spacemacs/braineo-editor/keybindings.el
+++ emacs/spacemacs/braineo-editor/keybindings.el
@@ -6,3 +6,10 @@
 (global-set-key (kbd "C-<f2>") 'bm-toggle)
 (global-set-key (kbd "<f2>") 'bm-next)
 (global-set-key (kbd "s-<f2>") 'bm-previous)
+
+;; fold
+(global-set-key (kbd "C-<tab>") 'evil-toggle-fold)
+
+;; anzu-mode enhances isearch & query-replace by showing total matches and current match position
+(global-set-key (kbd "M-%") 'anzu-query-replace)
+(global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
