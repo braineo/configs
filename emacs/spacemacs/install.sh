158f9755ce8a70ed9b6c47d9b3a013942c57b746
Ye Binbin <binbin.ye@mujin.co.jp>
2017-12-26 06:21:49 -0500

    Update spacemacs install script

--- emacs/spacemacs/install.sh
+++ emacs/spacemacs/install.sh
@@ -3,42 +3,22 @@
 
 set -e
 
-SCRIPT=$(basename $0)
-MODE=${1:-setup}
 
-function usage
-{
-    echo "Usage: ${SCRIPT} <mode>"
-    echo "       ${SCRIPT} setup/vanilla/guest"
-    echo "       setup: spacemacs is install at home, just need to symlink private layers and .spacemacs file"
-    echo "       vanilla: full install"
-    echo "       guest: full install to specific path to keep existing emacs config"
-}
-
-if ! [[ ${MODE} =~ ^(setup|vanilla|guest)$ ]]; then
-    usage
-fi
-
-# private layers to install
-declare -a layers=(braineo braineo-ui braineo-editor braineo-global-keybindings braineo-lang)
-declare -A map
-
-for key in "${!layers[@]}"; do map[${layers[$key]}]="$key"; done
-
-PRIVATEPATH=$(pwd)
-for layer in *; do
-    if [[ -n "${map[$layer]}" ]]; then
-        TARGET="${HOME}/.emacs.d/private/${layer}"
-        if [[ -e ${TARGET} ]]; then
-            rm -rf ${TARGET}
-        fi;
-        echo "Linking layer $layer"
-        ln -s "${PRIVATEPATH}/${layer}" ${TARGET}
-    fi;
-done
-
-if [[ -e "${HOME}/.spacemacs" ]]; then
-    rm "${HOME}/.spacemacs"
+if [ -f $HOME/.spacemacs ] || [ -h $HOME/.spacemacs ]; then
+    rm "$HOME/.spacemacs"
 fi;
-echo "Linking spacemacs config"
-ln -s "${PRIVATEPATH}/spacemacs" "${HOME}/.spacemacs"
+
+
+DOTSPACEMACSD=$HOME/.spacemacs.d
+
+if [ -d ${DOTSPACEMACSD} ]; then
+    if [ -h ${DOTSPACEMACSD} ]; then
+        # Remove symlink, no -rf cause it might delete contents it points to
+        rm ${DOTSPACEMACSD}
+    else
+        rm -rf ${DOTSPACEMACSD}
+    fi;
+fi;
+
+touch custom.el
+ln -s "$(pwd)/../spacemacs" "$HOME/.spacemacs.d"
