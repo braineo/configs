d93ded454b675ff9ff525f401e865093ab07506e
Ye Binbin <phantom2501@gmail.com>
2018-02-03 10:53:13 +0900

    Add transparent title bar

--- emacs/spacemacs/layers/braineo-ui/config.el
+++ emacs/spacemacs/layers/braineo-ui/config.el
@@ -3,6 +3,9 @@
 (set-frame-parameter (selected-frame) 'alpha '(95 95))
 (add-to-list 'default-frame-alist '(alpha 95 95))
 
+;; When it is macOS and verison number is higher than 26.0
+(when (and (spacemacs/system-is-mac) (>= emacs-major-version 26))
+  (setq default-frame-alist '((ns-transparent-titlebar . t) (ns-appearance . 'nil))))
 
 (defun on-frame-open (&optional frame)
   ;; "If the FRAME created in terminal don't load background color."
