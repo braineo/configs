9f5b4e273f537008b840f1bac85494b7a23c91e2
Binbin Ye <phantom2501@gmail.com>
2018-05-03 23:00:19 +0900

    Add LS_COLORS and enable coreutils

--- zsh/.zshell/config.zsh
+++ zsh/.zshell/config.zsh
@@ -32,3 +32,12 @@ setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
 setopt CDABLE_VARS          # Change directory to a path stored in a variable.
 setopt MULTIOS              # Write to multiple descriptors.
 setopt EXTENDED_GLOB        # Use extended globbing syntax.
+
+
+if [[ "$OSTYPE" == linux* ]]; then
+
+elif  [[ "$OSTYPE" = darwin* ]]; then
+# Some linux utils path
+    export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:/usr/local/opt/gnu-tar/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+    export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:/usr/local/opt/findutils/libexec/gnuman:/usr/local/opt/gnu-tar/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:MANPATH"
+fi
