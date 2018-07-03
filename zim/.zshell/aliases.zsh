48974615c6745ba3e33546e107a1fa2d4dd2b080
Binbin Ye <binbin.ye@mujin.co.jp>
2018-07-03 13:19:18 +0900

    Fix emacsapp alias

--- zim/.zshell/aliases.zsh
+++ zim/.zshell/aliases.zsh
@@ -1,6 +1,6 @@
 if [[ "$OSTYPE" == linux* ]]; then
     alias emacs="emacsclient -t"
-    alias emacsapp="export LC_CTYPE=zh_CN.UTF-8 /usr/local/bin/emacs"
+    alias emacsapp="LC_CTYPE=zh_CN.UTF-8 /usr/local/bin/emacs"
 elif  [[ "$OSTYPE" = darwin* ]]; then
     alias emacsapp="open -a /Applications/Emacs.app"
     alias emacsd="/usr/local/bin/emacs --daemon"
