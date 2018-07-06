333a630f40e6efc4446c8cd71f702efd891018f5
Binbin Ye <binbin.ye@mujin.co.jp>
2018-07-06 09:41:06 +0900

    Move keybinding setting to one file

--- spacemacs/.spacemacs.d/layers/braineo/keybindings.el
+++ spacemacs/.spacemacs.d/layers/braineo/keybindings.el
@@ -36,4 +36,76 @@
 (global-set-key (kbd (concat super-keybind-prefix "m l")) 'magit-log)
 (global-set-key (kbd (concat super-keybind-prefix "m f")) 'magit-log-buffer-file)
 (global-set-key (kbd (concat super-keybind-prefix "m b")) 'magit-blame)
+(global-set-key [remap kill-whole-line] 'crux-kill-whole-line)
 
+;; Align your code in a pretty way.
+(global-set-key (kbd "C-x \\") 'align-regexp)
+
+;; Window switching. (C-x o goes to the next window)
+(global-set-key (kbd "C-x O") (lambda ()
+                                (interactive)
+                                (other-window -1))) ;; back one
+
+;; Indentation help
+(global-set-key (kbd "C-^") 'crux-top-join-line)
+;; Start proced in a similar manner to dired
+(unless (eq system-type 'darwin)
+    (global-set-key (kbd "C-x p") 'proced))
+
+;; Start eshell or switch to it if it's active.
+(global-set-key (kbd "C-x m") 'eshell)
+
+;; Start a new eshell even if one is active.
+(global-set-key (kbd "C-x M") (lambda () (interactive) (eshell t)))
+
+;; Start a regular shell if you prefer that.
+(global-set-key (kbd "C-x M-m") 'shell)
+
+;; If you want to be able to M-x without meta
+(global-set-key (kbd "C-x C-m") 'smex)
+
+;; A complementary binding to the apropos-command (C-h a)
+(define-key 'help-command "A" 'apropos)
+
+;; A quick major mode help with discover-my-major
+(define-key 'help-command (kbd "C-m") 'discover-my-major)
+
+(define-key 'help-command (kbd "C-f") 'find-function)
+(define-key 'help-command (kbd "C-k") 'find-function-on-key)
+(define-key 'help-command (kbd "C-v") 'find-variable)
+(define-key 'help-command (kbd "C-l") 'find-library)
+
+(define-key 'help-command (kbd "C-i") 'info-display-manual)
+
+;; replace zap-to-char functionality with the more powerful zop-to-char
+(global-set-key (kbd "M-z") 'zop-up-to-char)
+(global-set-key (kbd "M-Z") 'zop-to-char)
+
+;; kill lines backward
+(global-set-key (kbd "C-<backspace>") (lambda ()
+                                        (interactive)
+                                        (kill-line 0)
+                                        (indent-according-to-mode)))
+
+;; Activate swoop easily inside isearch
+(define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+
+;; Better kill-ring
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+(global-set-key (kbd "C-c j") 'avy-goto-word-or-subword-1)
+(global-set-key (kbd (concat super-keybind-prefix ".")) 'avy-goto-word-or-subword-1)
+(global-set-key (kbd (concat super-keybind-prefix "w")) 'ace-window)
+
+;; Quick search and open file
+(global-set-key (kbd "C-.") 'helm-projectile-ag)
+(global-set-key (kbd "C-,") 'helm-projectile-find-file)
+
+;; Bookmark line and jump back and forth
+(global-set-key (kbd "C-<f2>") 'bm-toggle)
+(global-set-key (kbd "<f2>") 'bm-next)
