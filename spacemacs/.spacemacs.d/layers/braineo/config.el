9df5c3bad2c9841e840fb7083422635a1fb89c82
Binbin Ye <binbin.ye@mujin.co.jp>
2018-10-12 10:15:31 +0900

    avoid pyls error warnings

--- spacemacs/.spacemacs.d/layers/braineo/config.el
+++ spacemacs/.spacemacs.d/layers/braineo/config.el
@@ -12,3 +12,10 @@
     (lsp--set-configuration lsp-cfg)))
 
 (add-hook 'lsp-after-initialize-hook 'lsp-set-cfg)
+
+;;avoid error "Wrong type argument: sequencep" from `eldoc-message' if `lsp-enable-eldoc' is non-nil
+(setq lsp-enable-eldoc nil)
+;;avoid popup warning buffer if lsp can't found root directory (such as edit simple *.py file)
+(setq lsp-message-project-root-warning t)
+;;avoid error "Error from the Language Server: FileNotFoundError" if `create-lockfiles' is non-nil
+(setq create-lockfiles nil)
