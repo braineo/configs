31ca339fc7245e3a07fd0ff956deee868f8b240c
Binbin Ye <binbin.ye@mujin.co.jp>
2024-08-07 09:40:35 +0900

    remove emacs alias

--- zim/dot-zshell/aliases.zsh
+++ zim/dot-zshell/aliases.zsh
@@ -1,15 +1,3 @@
-if [[ "$OSTYPE" == linux* ]]; then
-    alias emacs="emacsclient -t -a ''"
-    alias emacsapp="LC_CTYPE=zh_CN.UTF-8 /usr/local/bin/emacs"
-elif  [[ "$OSTYPE" = darwin* ]]; then
-    alias emacsapp="open -a /Applications/Emacs.app"
-    alias emacsd="/usr/local/bin/emacs --daemon"
-    alias emacs="emacsclient -c -a emacs-cocoa"
-fi
-
-alias killemacs="emacsclient -e '(kill-emacs)'"
-
-
 #
 # git
 #
